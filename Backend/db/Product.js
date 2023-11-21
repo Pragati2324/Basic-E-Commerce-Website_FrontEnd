2a36637e05b573942d317c03fa2dd8b838eeadf2
Pragati2324 <60358227+Pragati2324@users.noreply.github.com>
2023-11-21 15:48:25 +0000

    Backend

---
+++ Backend/db/Product.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const productSchema=new mongoose.Schema({
+   name:String,
+   price:String,
+   category:String,
+   userId:String,
+   brand:String
+});
+module.exports=mongoose.model("products",productSchema);
---
+++ Backend/db/config.js
@@ -0,0 +1,2 @@
+const mongoose=require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.0")
